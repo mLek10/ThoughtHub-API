4ec5cb4c43698a5e8844b5f55c9ac98fe9d79ba3
Emily Caton <caton.emily@yahoo.com>
2023-10-30 23:13:20 -0500

    add connection reuired

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/userRoutes');
 const thoughtRoutes = require('./routes/thoughtRoutes');
+const connection = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,10 +12,7 @@ app.use(express.json());
 app.use('/users', userRoutes);
 app.use('/thoughts', thoughtRoutes);
 
-mongoose.connection.once('open', () => { // Use mongoose.connection
-  console.log('MongoDB database connection established successfully');
+
   app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
   });
-});
- 
